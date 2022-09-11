41290c753a17ab71addb3f0624f0e6ae6272b411
tomoesaturn <hotarutwitter@gmail.com>
2022-09-11 09:45:26 +0900

    gatsbyのコードを消してreactに置き換える

--- old_gatsby/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  siteUrl: "https://tomoesaturn2.gitlab.io/tool/",
-  siteTitle: "ついったー高度検索",
-  siteDescription:  "スマホでも高度な検索使いたい！",
-  siteImage: this.iconUrl,
-  iconUrl: "src/images/icon.png",
-};
-
-module.exports = config;
