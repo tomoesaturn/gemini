53d1d2abf36f6a1ecf94003f642c1d8be3a4bfde
tomoesaturn <hotarutwitter@gmail.com>
2022-09-11 11:39:44 +0900

    PWA化

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,25 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/gemini/",
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ついったー高度検索",
+        short_name: "ついったー高度検索",
+        description: "スマホでも高度な検索使いたい！",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
